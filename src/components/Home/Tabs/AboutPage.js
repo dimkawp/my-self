08059141572d2b372da4883715cb5b041f8669f6
dimkawp <dimkawp@gmail.com>
2018-06-11 12:48:46 +0300

    mySelf
    add form onChange event.

--- src/components/Home/Tabs/AboutPage.js
+++ src/components/Home/Tabs/AboutPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Form, Input, TextArea } from 'semantic-ui-react';
+import { Grid, Form, Input, TextArea, Button } from 'semantic-ui-react';
 import {Animated} from "react-animated-css";
 import './AboutPage.css';
 //icons
@@ -11,10 +11,31 @@ class AboutPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            formName: '',
+            formMail: '',
+            formText: ''
 
         };
     }
     componentWillMount() {
+    }
+
+    handleChangeTextMessage = (e) => {
+        this.setState({TextMessage: e.target.value});
+        console.log(this.state.TextMessage);
+    }
+    ChangeFormName = (event) => {
+        this.setState({formName: event.target.value});
+    }
+    ChangeFormMail = (event) => {
+        this.setState({formMail: event.target.value});
+    }
+    ChangeFormText = (event) => {
+        this.setState({formText: event.target.value});
+    }
+    handleSubmit = (event) => {
+        console.log('Name - ' + this.state.formName, 'Mail - ' + this.state.formMail, 'Text - ' + this.state.formText);
+        event.preventDefault();
     }
 
     render() {
@@ -25,12 +46,12 @@ class AboutPage extends Component {
                         <div className="MainContainer">
                             <div className="FirstContainer share">
                                 <h1>
-                                    <span id="one">W</span><span>h</span><span>e</span><span>n</span> <span>W</span><span>e</span> <span>T</span><span>a</span><span>l</span><span>k</span>
+                                    <span id="one">W</span><span>r</span><span>i</span><span>t</span><span>e</span> <span>m</span><span>e</span>
                                 </h1>
                             </div>
                             <div className="SecondContainer share">
                                 <h1>
-                                    <span>W</span><span>e</span> <span>M</span><span>o</span><span>v</span><span>e</span>
+                                    <span>I</span><span>f</span> <span>I</span><span>n</span><span>t</span><span>e</span><span>r</span><span>e</span><span>s</span><span>t</span><span>i</span><span>n</span><span>g</span>
                                 </h1>
                             </div>
                         </div>
@@ -49,9 +70,7 @@ class AboutPage extends Component {
                     <div className="TextInfoBlock">
                         <Grid centered>
                             <p>
-                                Where does it come from?
-                                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-                                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
                             </p>
                         </Grid>
                     </div>
@@ -59,12 +78,16 @@ class AboutPage extends Component {
                 <Animated animationIn="bounceInLeft" animationOut="fadeOut" animationInDelay={2000} isVisible={true}>
                     <div className="FormBlock">
                         <Grid centered>
-                            <Form>
+                            <Form onSubmit={this.handleSubmit}>
                                 <Form.Group widths='equal'>
-                                    <Form.Field control={Input} label='First name' placeholder='First name' />
-                                    <Form.Field control={Input} label='First name' placeholder='First name' />
+                                    <Form.Field control={Input} label='Name' placeholder='Name'  value={this.state.formName} onChange={this.ChangeFormName} />
+                                    <Form.Field control={Input} label='Mail' placeholder='Mail' value={this.state.formMail} onChange={this.ChangeFormMail}/>
                                 </Form.Group>
-                                <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' />
+                                <Form.Field control={TextArea} label='Text' placeholder='Tell us more about you...' value={this.state.formText} onChange={this.ChangeFormText}/>
+                                <div className="subBlock">
+                                    <Button type='submit'>Submit</Button>
+                                </div>
+
                             </Form>
                         </Grid>
                     </div>
