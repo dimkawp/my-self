8644f8d13ee76ea017bfcd41dec7d1da13c7c00e
dimkawp <dimkawp@gmail.com>
2019-07-11 16:13:16 +0300

    Add lazy load on pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import PrivateLayout from './PrivateLayout'
 import UserLayout from './UserLayout'
-//components
-import AdminContainer from '../Containers/AdminContainer/index'
-import HomeContainer from '../Containers/HomeContainer/index'
-import AboutContainer from '../Containers/AboutContainer/index'
+// Components
+// Lazy load
+const AdminContainer = lazy(() => import('../Containers/AdminContainer/index'));
+const HomeContainer = lazy(() => import('../Containers/HomeContainer/index'));
+const AboutContainer = lazy(() => import('../Containers/AboutContainer/index'));
 
 class App extends Component {
   render() {
     return (
     <Router exact path='/' component={HomeContainer}>
       <Switch>
-        <PrivateLayout exact path="/admin" component={AdminContainer}/>
-        <UserLayout path="/about" component={AboutContainer}/> 
-        <UserLayout path="/home" component={HomeContainer}/>    
-        <UserLayout path="/" component={HomeContainer}/>
+        <PrivateLayout exact path="/admin" component={WaitingComponent(AdminContainer)}/>
+        <UserLayout path="/about" component={WaitingComponent(AboutContainer)}/> 
+        <UserLayout path="/home" component={WaitingComponent(HomeContainer)}/>    
+        <UserLayout path="/" component={WaitingComponent(HomeContainer)}/>
       </Switch>
     </Router>
     );
   }
+}
+
+function WaitingComponent(Component) {
+  return props => (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component {...props} />
+    </Suspense>
+  );
 }
 
 export default App;
